9857f0f8056141d5d7a9f716c8b40247a6bd160e
Ricky Bakersmith <90644027+RickyB923@users.noreply.github.com>
2022-02-28 21:00:33 -0600

    Added answer detector script

--- Assets/Scripts/AnswerDetector.cs
+++ Assets/Scripts/AnswerDetector.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AnswerDetector : MonoBehaviour
@@ -8,6 +9,10 @@ public class AnswerDetector : MonoBehaviour
 
     public GameObject[] currentRow;
     public GameObject[] answerKey;
+    [SerializeField] GameObject[] pins;
+    [SerializeField] GameObject hintGrid;
+
+    GameObject[] pinSlots;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,6 +44,44 @@ public class AnswerDetector : MonoBehaviour
             currentRowMats[i] = temp;
         }
 
-        int xx = 13;
+        Report(answerMats, currentRowMats);
+    }
+
+    void Report(Material[] answerMats, Material[] currentMats)
+    {
+        int[] answerValues = new int[currentMats.Length];
+        List<Material> compMats = answerMats.ToList();
+        List<Color> colorAnswers = new List<Color>();
+
+        foreach (var item in compMats)
+        {
+            colorAnswers.Add(item.color);
+        }
+
+        for (int i = 0; i < currentMats.Length; i++)
+        {
+            if (currentMats[i].color == answerMats[i].color)
+            {
+                answerValues[i] = 1;
+                InstantiatePin(0, hintGrid.transform.GetChild(i).transform);
+            }
+            else if (colorAnswers.Contains(currentMats[i].color))
+            {
+                answerValues[i] = 0;
+                InstantiatePin(1, hintGrid.transform.GetChild(i).transform);
+            }
+            else
+            {
+                answerValues[i] = -1;
+            }
+            Debug.Log(answerValues[i]);
+        }
+    }
+
+    void InstantiatePin(int index, Transform transform)
+    {
+        GameObject pin = Instantiate(pins[index]);
+        pin.transform.position = transform.position;
     }
 }
+
